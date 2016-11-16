60119ac53c9718da2e0c2552b84ca3503a72a976
Nikolas Yanez <yaneznikolas@gmail.com>
2016-11-16 17:13:17 -0500

    fixed edit modal

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,6 +2,9 @@
 // All this logic will automatically be available in application.js.
 
 $(document).on('turbolinks:load', function() {
-  console.log('document is ready!', new Date()); // this logging is optional
   $('#task_name').focus();
+
+  $('.modal-footer .btn-primary').click(function() {
+    $('.edit_task').submit();
+  });
 });
